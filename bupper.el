639ccf7fa4d24f185c9e8537147055898c0cd533
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-09-21 15:38:15 +0200

    Ensure writable buffer and enough space for overlay
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- bupper.el
+++ bupper.el
@@ -40,23 +40,66 @@
   "Face used to highlight bupper window ID numbers."
   :group 'cursor-flash)
 
+(defmacro bupper--with-variable (var value &rest body)
+  "Run `BODY` with the symbol `VAR` temporary set to `VALUE`."
+  `(let ((original-value ,var))
+    (setq ,var ,value)
+    (progn ,@body)
+    (setq ,var original-value)))
+
+(defun bupper--ensure-enough-space (required-space)
+  "Ensure that at least `REQUIRED-SPACE` columns are available in the current line."
+  (save-mark-and-excursion
+    (end-of-line)
+    (let* ((available-space (current-column))
+           (missing-space (- required-space available-space)))
+      (when (> missing-space 0)
+        (insert (make-string missing-space ? ))))))
+
+(defun bupper--backup-content ()
+  "Backup content of the windows."
+  (let ((content-map (make-hash-table :test 'equal)))
+    (dolist (window (window-list))
+      (with-current-buffer (window-buffer window)
+        (puthash (buffer-name) (list (point) (buffer-substring (point-min) (point-max))) content-map)))
+    content-map))
+
+(defun bupper--restore-content (content-map)
+  "Restore content of the windows from the `CONTENT-MAP` mapping."
+  (dolist (window (window-list))
+    (with-current-buffer (window-buffer window)
+      (bupper--with-variable
+       inhibit-read-only t
+       (let* ((value (gethash (buffer-name) content-map))
+              (pos (pop value))
+              (content (pop value)))
+         (save-mark-and-excursion
+           (goto-char (point-min))
+           (delete-region (point-min) (point-max))
+           (insert content)
+           (set-buffer-modified-p nil))
+         (set-window-point window pos))))))
+
 (defun bupper--add-string-overlay-to-window (window string)
   "Add the STRING `string` as overlay in `WINDOW` in the first visible position."
   (with-current-buffer (window-buffer window)
-    (setq string (concat " " string " "))
-    (save-mark-and-excursion
-      (let ((pos (window-start window))
-            (width (length string))
-            ov)
-        (let ((empty-row (make-string width ? )))
-          (dotimes (i 3)
-            (goto-char pos)
-            (when (> 2 (progn (end-of-line) (current-column)))
-              (insert empty-row))
-            (setq ov (make-overlay pos (+ 3 (goto-char pos))))
-            (overlay-put ov 'face 'bupper-face)
-            (overlay-put ov 'display (if (= i 1) string empty-row))
-            (setq pos (+ 1 (line-end-position)))))))))
+    (bupper--with-variable
+     inhibit-read-only t
+     (setq string (concat " " string " "))
+     (save-mark-and-excursion
+       (let ((pos (window-start window))
+             (width (length string))
+             ov)
+         (let ((empty-row (make-string width ? )))
+           (dotimes (i 3)
+             (goto-char pos)
+             (bupper--ensure-enough-space width)
+             (let ((start pos)
+                   (end (+ width (goto-char pos))))
+               (setq ov (make-overlay start end))
+               (overlay-put ov 'face 'bupper-face)
+               (overlay-put ov 'display (if (= i 1) string empty-row))
+               (setq pos (+ 1 (line-end-position)))))))))))
 
 (defun bupper--remove-overlays-from-window (window)
   "Remove all overlays from `WINDOW."
@@ -88,14 +131,16 @@
 (defun bupper-swap ()
   "Prompt user to swap the buffer with the one of another window."
   (interactive)
-  (bupper--add-string-overlays)
-  (unwind-protect
-   (let ((target-window (bupper--prompt-target-window)))
-     (let ((target-buffer (window-buffer target-window)))
-       (set-window-buffer target-window (current-buffer))
-       (set-window-buffer nil target-buffer)
-       (select-window target-window)))
-   (bupper--remove-string-overlays)))
+  (let ((content-map (bupper--backup-content)))
+    (bupper--add-string-overlays)
+    (unwind-protect
+        (let ((target-window (bupper--prompt-target-window)))
+          (let ((target-buffer (window-buffer target-window)))
+            (set-window-buffer target-window (current-buffer))
+            (set-window-buffer nil target-buffer)
+            (select-window target-window)))
+      (bupper--remove-string-overlays)
+      (bupper--restore-content content-map))))
 
 (provide 'bupper)
 
